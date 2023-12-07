b921f0cb4f72ec62bdd129fea12d7057cfd2c3be
Vaibhav Udaywal <vaibhav.udaywal@simpplr.com>
2023-12-07 22:08:59 +0530

    Initial Commit

---
+++ onething/onethingApp.swift
@@ -0,0 +1,17 @@
+//
+//  onethingApp.swift
+//  onething
+//
+//  Created by Vaibhav Udaywal on 07/12/23.
+//
+
+import SwiftUI
+
+@main
+struct onethingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
